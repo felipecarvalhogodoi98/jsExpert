ca7e2cd6c698f2405dccd44756521967a9b990a7
felipecarvalhogodoi98 <felipecarvalhogodoi98@gmail.com>
2022-11-17 08:22:50 -0300

    add TDD aula05

---
+++ testing/aula05-tdd-project/src/service/carService.js
@@ -0,0 +1,72 @@
+const BaseRepository = require("../repository/base/baseRepository");
+const Tax = require("./../entities/tax");
+const Transaction = require("../entities/transaction");
+
+class CarService {
+  constructor({ cars }) {
+    this.carRepository = new BaseRepository({ file: cars });
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge;
+    this.currencyFormat = new Intl.NumberFormat("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
+
+  getRandomPositionFromArray(list) {
+    const listLength = list.length;
+
+    return Math.floor(Math.random() * listLength);
+  }
+
+  chooseRandomCar(carCategory) {
+    const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds);
+    const carId = carCategory.carIds[randomCarIndex];
+
+    return carId;
+  }
+
+  async getAvailableCar(carCategory) {
+    const carId = this.chooseRandomCar(carCategory);
+    const car = await this.carRepository.find(carId);
+
+    return car;
+  }
+
+  async calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer;
+    const { price } = carCategory;
+    const { then: tax } = this.taxesBasedOnAge.find(
+      (tax) => tax.from <= age && tax.to >= age
+    );
+
+    const finalPrice = tax * price * numberOfDays;
+    return this.currencyFormat.format(finalPrice);
+  }
+
+  async rent(customer, carCategory, numberOfDays) {
+    const car = await this.getAvailableCar(carCategory);
+    const finalPrice = await this.calculateFinalPrice(
+      customer,
+      carCategory,
+      numberOfDays
+    );
+
+    const today = new Date();
+    today.setDate(today.getDate() + numberOfDays);
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const dueDate = today.toLocaleDateString("pt-br", options);
+
+    return new Transaction({
+      car,
+      customer,
+      dueDate,
+      amount: finalPrice,
+    });
+  }
+}
+
+module.exports = CarService;
